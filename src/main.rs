7278dae9d814920da8d805af6ea06bb2067756df
sawyer bristol <sawyerbristol@gmail.com>
2025-04-17 07:59:33 -0600

    first commit

---
+++ src/main.rs
@@ -0,0 +1,226 @@
+#![no_std]
+#![no_main]
+#![feature(impl_trait_in_assoc_type)]
+
+use defmt::info;
+use embassy_executor::Spawner;
+use embassy_rp::bind_interrupts;
+use embassy_rp::clocks::clk_sys_freq;
+use embassy_rp::gpio::{Level, Output};
+use embassy_rp::peripherals::PIO0;
+use embassy_rp::pio::program::{
+    Assembler, InSource, MovDestination, MovOperation, MovSource, OutDestination, SideSet,
+};
+use embassy_rp::pio::{
+    self, Common, Direction, Instance, InterruptHandler, LoadedProgram, Pio, PioPin, ShiftConfig,
+    ShiftDirection, StateMachine,
+};
+use embassy_rp::spi::{self, Error, Phase, Polarity};
+use embedded_hal::spi::SpiBus;
+use embedded_hal_bus::spi::ExclusiveDevice;
+use embedded_sdmmc::{SdCard, TimeSource, Timestamp};
+use {defmt_rtt as _, panic_probe as _};
+
+bind_interrupts!(struct Irqs {
+    PIO0_IRQ_0 => InterruptHandler<PIO0>;
+});
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    info!("Running");
+    let p = embassy_rp::init(Default::default());
+    let Pio {
+        mut common, sm0, ..
+    } = Pio::new(p.PIO0, Irqs);
+
+    let mut config = spi::Config::default();
+    config.frequency = 400_000;
+    let programs = PioSpiPrograms::new(&mut common, &config);
+    let spi = PioSpi::new(
+        &mut common,
+        sm0,
+        p.PIN_2,
+        p.PIN_3,
+        p.PIN_4,
+        programs,
+        config,
+    );
+
+    let cs = Output::new(p.PIN_5, Level::High);
+    let spi_dev = ExclusiveDevice::new_no_delay(spi, cs).unwrap();
+
+    let sdcard = SdCard::new(spi_dev, embassy_time::Delay);
+    info!("card type: {}", sdcard.get_card_type());
+
+    let mut volume_mgr = embedded_sdmmc::VolumeManager::new(sdcard, DummyTimeSource());
+
+    let mut volume0 = volume_mgr
+        .open_volume(embedded_sdmmc::VolumeIdx(0))
+        .unwrap();
+    info!("Volume 0: {:?}", defmt::Debug2Format(&volume0));
+
+    // Open the root directory (mutably borrows from the volume).
+    let mut root_dir = volume0.open_root_dir().unwrap();
+
+    root_dir
+        .iterate_dir(|file| info!("File: {}", file.name))
+        .unwrap();
+
+    info!("Done!!!");
+
+    loop {}
+}
+
+fn polarity_bit(polarity: Polarity, on: bool) -> u8 {
+    match polarity {
+        Polarity::IdleLow => match on {
+            true => 1,
+            false => 0,
+        },
+        Polarity::IdleHigh => match on {
+            true => 0,
+            false => 1,
+        },
+    }
+}
+
+pub struct PioSpiPrograms<'d, PIO: Instance> {
+    prg: LoadedProgram<'d, PIO>,
+}
+
+impl<'d, PIO: Instance> PioSpiPrograms<'d, PIO> {
+    pub fn new(pio: &mut Common<'d, PIO>, config: &spi::Config) -> Self {
+        let mut assembler = Assembler::<32>::new_with_side_set(SideSet::new(false, 1, false));
+        let pol = config.polarity;
+
+        match config.phase {
+            Phase::CaptureOnFirstTransition => {
+                assembler.out_with_delay_and_side_set(
+                    OutDestination::PINS,
+                    1,
+                    1,
+                    polarity_bit(pol, false),
+                );
+                assembler.in_with_delay_and_side_set(InSource::PINS, 1, 1, polarity_bit(pol, true));
+            }
+            Phase::CaptureOnSecondTransition => {
+                assembler.out_with_side_set(OutDestination::X, 1, polarity_bit(pol, false));
+                assembler.mov_with_delay_and_side_set(
+                    MovDestination::PINS,
+                    MovOperation::None,
+                    MovSource::X,
+                    1,
+                    polarity_bit(pol, true),
+                );
+                assembler.in_with_side_set(InSource::PINS, 1, polarity_bit(pol, false));
+            }
+        }
+
+        Self {
+            prg: pio.load_program(&assembler.assemble_program()),
+        }
+    }
+}
+
+pub struct PioSpi<'d, PIO: Instance, const SM: usize> {
+    sm: StateMachine<'d, PIO, SM>,
+}
+
+impl<'d, PIO: Instance, const SM: usize> PioSpi<'d, PIO, SM> {
+    pub fn new(
+        pio: &mut Common<'d, PIO>,
+        mut sm: StateMachine<'d, PIO, SM>,
+        clk_pin: impl PioPin,
+        mosi_pin: impl PioPin,
+        miso_pin: impl PioPin,
+        programs: PioSpiPrograms<'d, PIO>,
+        config: spi::Config,
+    ) -> Self {
+        let clk = pio.make_pio_pin(clk_pin);
+        let mosi = pio.make_pio_pin(mosi_pin);
+        let miso = pio.make_pio_pin(miso_pin);
+
+        let mut cfg = pio::Config::default();
+        cfg.use_program(&programs.prg, &[&clk]);
+        cfg.set_out_pins(&[&mosi]);
+        cfg.set_in_pins(&[&miso]);
+        cfg.clock_divider = ((clk_sys_freq() / config.frequency) as u16).into();
+        cfg.shift_in = ShiftConfig {
+            threshold: 32,
+            direction: ShiftDirection::Right,
+            auto_fill: true,
+        };
+        cfg.shift_out = ShiftConfig {
+            threshold: 32,
+            direction: ShiftDirection::Left,
+            auto_fill: true,
+        };
+        sm.set_config(&cfg);
+        sm.set_enable(true);
+        sm.set_pin_dirs(Direction::Out, &[&clk]);
+        sm.set_pin_dirs(Direction::In, &[&miso]);
+
+        Self { sm }
+    }
+}
+
+impl<'d, T: Instance, const SM: usize> embedded_hal::spi::ErrorType for PioSpi<'d, T, SM> {
+    type Error = Error;
+}
+
+impl<'a, PIO: Instance, const SM: usize> SpiBus<u8> for PioSpi<'a, PIO, SM> {
+    fn read(&mut self, words: &mut [u8]) -> Result<(), Self::Error> {
+        for word in words.iter_mut() {
+            self.write(&[0x00])?;
+            while self.sm.rx().empty() {}
+            *word = self.sm.rx().pull() as u8;
+        }
+        Ok(())
+    }
+
+    fn write(&mut self, words: &[u8]) -> Result<(), Self::Error> {
+        for &byte in words {
+            while self.sm.tx().full() {}
+
+            self.sm.tx().push(byte as u32);
+        }
+
+        self.flush()
+    }
+
+    fn transfer(&mut self, read: &mut [u8], write: &[u8]) -> Result<(), Self::Error> {
+        for (r, &w) in read.iter_mut().zip(write.iter()) {
+            while self.sm.tx().full() {}
+            self.sm.tx().push(w as u32);
+
+            while self.sm.rx().empty() {}
+            *r = self.sm.rx().pull() as u8;
+        }
+
+        Ok(())
+    }
+
+    fn transfer_in_place(&mut self, words: &mut [u8]) -> Result<(), Self::Error> {
+        for word in words.iter_mut() {
+            while self.sm.tx().full() {}
+            self.sm.tx().push(*word as u32);
+
+            while self.sm.rx().empty() {}
+            *word = self.sm.rx().pull() as u8;
+        }
+
+        Ok(())
+    }
+
+    fn flush(&mut self) -> Result<(), Self::Error> {
+        while !self.sm.tx().empty() {}
+        Ok(())
+    }
+}
+
+pub struct DummyTimeSource();
+impl TimeSource for DummyTimeSource {
+    fn get_timestamp(&self) -> Timestamp {
+        Timestamp::from_calendar(2022, 1, 1, 0, 0, 0).unwrap()
+    }
+}
